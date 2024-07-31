9662168c2dc89722341dd49721a3761b0b768ff2
kky-ykk <kishan.linkedin.kky@gmail.com>
2024-07-31 18:02:26 +0530

    server cors

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
-const express=require("express");
-const User=require("./models/user");
-const path=require("path");
+const express = require("express");
+const User = require("./models/user");
+const path = require("path");
 
-const cors=require("cors");
+const cors = require("cors");
 
 require('dotenv').config();
 require("./db");
 
-const app=express();
+const app = express();
 
-app.use(cors());
+// Configure CORS to allow requests from specific origin
+app.use(cors({
+  origin: 'https://usermanagement-0idw.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}));
 
 app.use(express.json());
 
-// app.use("/",(req,res)=>{
-//     try {
-//         res.status(200).json({message:"Welcome to user management!"});
-//     } catch (error) {
-//         res.status(500).jsonp({message:"Internal server error"});
-//     }
-// });
+const userRoutes = require("./routes/userRoutes");
 
-const userRoutes=require("./routes/userRoutes");
+app.use("/user", userRoutes);
 
-app.use("/user",userRoutes);
+app.use(express.static(path.join(__dirname, 'ClientUI/dist')));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, 'ClientUI/dist/index.html')));
 
-
-app.use(express.static(path.join(__dirname,'ClientUI/dist')));
-app.get("*",(req,res)=>res.sendFile(path.join(__dirname,'ClientUI/dist/index.html')))
-
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log("Listening to PORT :",PORT);
-})
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Listening to PORT :", PORT);
+});
